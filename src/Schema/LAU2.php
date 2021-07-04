9500a826b08ab7e2fe74eb00bec8b1629f1902bb
Jan Jablunka <jablunka@gmail.com>
2021-07-04 10:40:30 +0200

    incremental

--- src/Schema/LAU2.php
+++ src/Schema/LAU2.php
@@ -112,7 +112,7 @@ class LAU2 extends \Heimat\SchemaObject
 		$string = (new TString($value))->normalizeSpaces();
 		$string = preg_replace('/&nbsp;/', '', $string);
 
-		$items = preg_split('/[,\/]/', $string);
+		$items = preg_split('/(,|\/|\sa\s)/', $string);
 		$items = array_map('trim', $items);
 
 		$postalCodeRegexp = '[0-9]\s*[0-9]\s*[0-9]\s*[0-9]\s*[0-9]';
